08b0809dd97625278bed302dd3227ced58813f40
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-04-19 13:25:02 +0530

    Add project files.

---
+++ Inherit/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inherit
+{
+     class Class1:Details
+    {
+        public void Adderess()
+        {
+            string Address = "Hyderabad";
+            Console.WriteLine("The Address is :"+ Address);
+        }
+        static void Main(string[] args)
+        {
+            Details d; //Initialized but don't have memory.
+            Class1 c=new Class1(); //Initialized and having memory location
+            d = c;
+            c.Names();
+            d.Names(); Console.WriteLine("-------------------");
+            Console.ReadLine();
+
+            d.Descriptions();
+            c.Descriptions();
+            Console.WriteLine("-------------------");
+            Console.ReadLine();
+
+            c.Adderess();
+            Console.WriteLine("-------------------");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Inherit/Details.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inherit
+{
+    public class Details
+    {
+        //public Details()
+        //{
+        //    Console.WriteLine("constructor is calling");
+        //    Console.ReadLine();
+        //}
+        public void Names()
+        {
+            string Fname = "Sanjay";
+            string Lname = "Kumar";
+            Console.WriteLine("The Name of the Candidate is :" +Fname+" "+ Lname);
+        }
+        public void Descriptions()
+        {
+            string Qualification = "MTech";
+            Console.WriteLine("Qualification is :" +Qualification);
+            string Description = "QA Lead";
+            Console.WriteLine("Description is :" +Description);
+
+        }
+
+    }
+}
